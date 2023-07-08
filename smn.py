d6b33ac956794f99eef97a35f38714f8fa8ddced
De <spark49942@gmail.com>
2023-07-09 01:04:51 +0300

    DZ8

--- smn.py
+++ smn.py
@@ -1,72 +1,103 @@
-# 1
-def newstr(text, nid):
-    for l in text.split():
-        nid += 1
-        print(f'{nid}. {l}')
+# # 1
+# def newstr(text, nid):
+#     for l in text.split():
+#         nid += 1
+#         print(f'{nid}. {l}')
 
-nid = 0
-text = input("1. Здравствуйте. Введите желаемый текст\n: ")
-print("\nРезультат:")
-newstr(text, nid)
+# nid = 0
+# text = input("1. Здравствуйте. Введите желаемый текст\n: ")
+# print("\nРезультат:")
+# newstr(text, nid)
 
-# 2
-def codunic(text, data = None):
-    if data == None:
-        data = list()
-    for l in text:
-        data.append(ord(l))
-    return data
+# # 2
+# def codunic(text, data = None):
+#     if data == None:
+#         data = list()
+#     for l in text:
+#         data.append(ord(l))
+#     return data
 
-text = list(input("\n2. Введите желаемый текст\n: "))
-res = sorted(codunic(text), reverse=True)
-print(res)
+# text = list(input("\n2. Введите желаемый текст\n: "))
+# res = sorted(codunic(text), reverse=True)
+# print(res)
 
-# 3
-def unidi(unter, uber, data = None):
-    if data == None:
-        data = dict()
-    for i in range(unter, uber + 1):
-        data[chr(i)] = i
-    return data
+# # 3
+# def unidi(unter, uber, data = None):
+#     if data == None:
+#         data = dict()
+#     for i in range(unter, uber + 1):
+#         data[chr(i)] = i
+#     return data
 
-digs = input("\n3. Введите два числа через пробел\n: ").split()
-digs = sorted(digs)
-res = unidi(int(digs[0]), int(digs[len(digs) - 1]))
-print(res)
+# digs = input("\n3. Введите два числа через пробел\n: ").split()
+# digs = sorted(digs)
+# res = unidi(int(digs[0]), int(digs[len(digs) - 1]))
+# print(res)
 
-# 4
-def digsort(digs):
-    for i in range(len(digs)):
-        for j in range(len(digs)):
-            if digs[i] > digs[j]:
-                temp = digs[j]
-                digs[j] = digs[i]
-                digs[i] = temp
-    return digs
+# # 4
+# def digsort(digs):
+#     for i in range(len(digs)):
+#         for j in range(len(digs)):
+#             if digs[i] > digs[j]:
+#                 temp = digs[j]
+#                 digs[j] = digs[i]
+#                 digs[i] = temp
+#     return digs
 
-digs = [4, 8, 15, 16, 23, 42]
-digs = input("\n4. Введите два числа через пробел\n: ").split()
-res = digsort(digs)
-print(res)
+# digs = [4, 8, 15, 16, 23, 42]
+# digs = input("\n4. Введите два числа через пробел\n: ").split()
+# res = digsort(digs)
+# print(res)
 
-# 5
-print("\n5. ")
-def worldparty(n, p, l, fin = None):
-    # for nl in range(len(l)):
-    #     for ln in range(len(l[nl])):
-    #         if l[nl][ln] == '%':
-    #             l[nl][ln].replace('%', '')
-    l = [l.replace('%', '') for l in levelup]
-    print(l)
-    if fin == None:
-        fin = dict()
-    for i in range(len(payday)):
-        fin[n[i]] = p[i] * (float(l[i]) / 100)
-    return fin
+# # 5
+# print("\n5. ")
+# def worldparty(n, p, l, fin = None):
+#     l = [l.replace('%', '') for l in levelup]
+#     print(l)
+#     if fin == None:
+#         fin = dict()
+#     for i in range(len(payday)):
+#         fin[n[i]] = p[i] * (float(l[i]) / 100)
+#     return fin
 
-names = ["Agent Dennis", "Killing Spree", "Hearr Icks", "Puppet Master", "Jake El"]
-payday = [500, 1000, 200, 360, 800]
-levelup = ["10.17%", "17.03%", "100.0%", "6.66%", "13.07%"]
-# levelup = [l.replace('%', '') for l in levelup]
-res = worldparty(names, payday, levelup)
-print(res)
+# names = ["Agent Dennis", "Killing Spree", "Hearr Icks", "Puppet Master", "Jake El"]
+# payday = [500, 1000, 200, 360, 800]
+# levelup = ["10.17%", "17.03%", "100.0%", "6.66%", "13.07%"]
+# res = worldparty(names, payday, levelup)
+# print(res)
+
+# 6
+def aplusi(ar, ray, ind, dex):
+    if ind > dex:
+        temp = dex
+        dex = ind
+        ind = temp
+    if ind < -len(ar):
+        ind = 0
+    if dex > len(ar):
+        dex = len(ar)
+    for i in range (ind, dex):
+        print(f"{ar[i]} + {ray[i]} = {ar[i] + ray[i]}")
+
+ar = [1, 3, 7, 15]
+ray = [2, 6, 14, 22]
+ind = int(input(f"6. Введите последовательно первый и второй индексы (от {-len(ar)} до {len(ar)})\n: "))
+dex = int(input(": "))
+aplusi(ar, ray, ind, dex)
+
+# 7
+business = {
+    'Team 17': 2,
+    'COMRA': 3,
+    'A-B06M24 Gang': 4,
+    'Hellbound Razers': 6,
+    'The Blues Hood': 777
+}
+
+# 8
+print("\n8. ")
+def worldparty(team, fin = None):
+    team = [team.replace('s', '') for team in team]
+    print(team)
+
+team = {"Agent Dennis", "Spadge", "Boggy B", "Dr. Awesome", "Chicken Legs", "Tapper", "Andy D", "Seventies Babe"}
