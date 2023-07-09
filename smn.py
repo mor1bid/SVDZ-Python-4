b47fb791c6efa07693c4b9cf7322bd09800fa796
De <spark49942@gmail.com>
2023-07-09 03:02:11 +0300

    DZ-smnf

--- smn.py
+++ smn.py
@@ -1,72 +1,72 @@
 import random
 
-# # 1
-# def newstr(text, nid):
-#     for l in text.split():
-#         nid += 1
-#         print(f'{nid}. {l}')
+# 1
+def newstr(text, nid):
+    for l in text.split():
+        nid += 1
+        print(f'{nid}. {l}')
 
-# nid = 0
-# text = input("1. Здравствуйте. Введите желаемый текст\n: ")
-# print("\nРезультат:")
-# newstr(text, nid)
+nid = 0
+text = input("1. Здравствуйте. Введите желаемый текст\n: ")
+print("\nРезультат:")
+newstr(text, nid)
 
-# # 2
-# def codunic(text, data = None):
-#     if data == None:
-#         data = list()
-#     for l in text:
-#         data.append(ord(l))
-#     return data
+# 2
+def codunic(text, data = None):
+    if data == None:
+        data = list()
+    for l in text:
+        data.append(ord(l))
+    return data
 
-# text = list(input("\n2. Введите желаемый текст\n: "))
-# res = sorted(codunic(text), reverse=True)
-# print(res)
+text = list(input("\n2. Введите желаемый текст\n: "))
+res = sorted(codunic(text), reverse=True)
+print(res)
 
-# # 3
-# def unidi(unter, uber, data = None):
-#     if data == None:
-#         data = dict()
-#     for i in range(unter, uber + 1):
-#         data[chr(i)] = i
-#     return data
+# 3
+def unidi(unter, uber, data = None):
+    if data == None:
+        data = dict()
+    for i in range(unter, uber + 1):
+        data[chr(i)] = i
+    return data
 
-# digs = input("\n3. Введите два числа через пробел\n: ").split()
-# digs = sorted(digs)
-# res = unidi(int(digs[0]), int(digs[len(digs) - 1]))
-# print(res)
+digs = input("\n3. Введите два числа через пробел\n: ").split()
+digs = sorted(digs)
+res = unidi(int(digs[0]), int(digs[len(digs) - 1]))
+print(res)
 
-# # 4
-# def digsort(digs):
-#     for i in range(len(digs)):
-#         for j in range(len(digs)):
-#             if digs[i] > digs[j]:
-#                 temp = digs[j]
-#                 digs[j] = digs[i]
-#                 digs[i] = temp
-#     return digs
+# 4
+def digsort(digs):
+    for i in range(len(digs)):
+        for j in range(len(digs)):
+            if digs[i] > digs[j]:
+                temp = digs[j]
+                digs[j] = digs[i]
+                digs[i] = temp
+    return digs
 
-# digs = [4, 8, 15, 16, 23, 42]
-# digs = input("\n4. Введите два числа через пробел\n: ").split()
-# res = digsort(digs)
-# print(res)
+digs = [4, 8, 15, 16, 23, 42]
+digs = input("\n4. Введите два числа через пробел\n: ").split()
+res = digsort(digs)
+print(res)
 
-# # 5
-# print("\n5. ")
-# def worldparty(n, p, l, fin = None):
-#     l = [l.replace('%', '') for l in levelup]
-#     print(l)
-#     if fin == None:
-#         fin = dict()
-#     for i in range(len(payday)):
-#         fin[n[i]] = p[i] * (float(l[i]) / 100)
-#     return fin
+# 5
+print("\n5. ")
+def worldparty(n, p, l, fin = None):
+    l = [l.replace('%', '') for l in levelup]
+    print(l)
+    if fin == None:
+        fin = dict()
+    for i in range(len(payday)):
+        fin[n[i]] = p[i] * (float(l[i]) / 100)
+    return fin
 
-# names = ["Agent Dennis", "Killing Spree", "Hearr Icks", "Puppet Master", "Jake El"]
-# payday = [500, 1000, 200, 360, 800]
-# levelup = ["10.17%", "17.03%", "100.0%", "6.66%", "13.07%"]
-# res = worldparty(names, payday, levelup)
-# print(res)
+names = ["Agent Dennis", "Killing Spree", "Hearr Icks", "Puppet Master", "Jake El"]
+payday = [500, 1000, 200, 360, 800]
+levelup = ["10.17%", "17.03%", "100.0%", "6.66%", "13.07%"]
+res = worldparty(names, payday, levelup)
+print(res)
 
 # 6
 def aplusi(ar, ray, ind, dex):
@@ -92,7 +92,8 @@ else:
 
 # 7
 print("\n7. ")
-def stonksnot(b, st, nst, bank, check):
+def stock(b):
+    check = True
     for i in b:
         bank = 0
         st = 0
@@ -103,11 +104,11 @@ def stonksnot(b, st, nst, bank, check):
             else:
                 nst += b[i][j]
         bank = st - nst
+        print(f'\n{i} имеют:\nДоходы в размере {st} кредитов\nРасходы в размере {nst} кредитов\nПрибыль в размере {bank} кредитов')
         if bank < 0:
             check = False
-        return bank, check
+    return check
 
-check = True
 business = {
     'Team 17': [random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000)], 
     'COMRA': [random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000)],
@@ -115,17 +116,18 @@ business = {
     'Hellbound Razers': [random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000)],
     'The Blues Hood': [random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000), random.randint(500, 1000)]
 }
-res = stonksnot(business, 0, 0, 0, check)
-print(res, "\nВсе бизнесы прибыльны?\n", check)
+res = stock(business)
+print("\nВсе бизнесы прибыльны?\n", res)
 
 # 8
 print("\n8. ")
-def armageddon(team, fin = None):
+def armageddon(team):
     for name in range(len(team)):
         if 's' in team[name][len(team[name]) - 1]:
             team[name] = None
     return team
 
 team = ["Agent Dennis", "Spadge", "Boggy B", "Dr. Awesome", "Chicken Legs", "Tapper", "Andy D", "Seventies Babe"]
+print(team)
 res = armageddon(team)
-print(f"{team}\n{res}")
+print(res)
